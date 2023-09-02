8a7f9cd5fabbcc8eb5a75d29c3fc6edabf714665
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-09-02 14:14:45 +0530

    complete

--- lib/view/Home/home_screen.dart
+++ lib/view/Home/home_screen.dart
@@ -41,88 +41,92 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                   )
-                : ListView.separated(
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 15),
-                    itemCount: Constant.toDoModelList.length,
-                    itemBuilder: (context, index) => Container(
-                      width: double.infinity,
-                      height: screenHeight / 4.3,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border:
-                              Border.all(color: Colors.deepPurple, width: 2.5)),
-                      child: Padding(
-                        padding: EdgeInsets.all(screenWidth / 20),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Title: ${Constant.toDoModelList[index].title}",
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  fontSize: 22),
-                            ),
-                            SizedBox(
-                              height: screenHeight / 50,
-                            ),
-                            Text(
-                              "content: ${Constant.toDoModelList[index].content}",
-                              // toDoModelList[index].content!,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black,
-                                  fontSize: 15),
-                            ),
-                            Text(
-                              "time: ${Constant.toDoModelList[index].time}",
-                              // toDoModelList[index].content!,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black,
-                                  fontSize: 15),
-                            ),
-                            SizedBox(
-                              height: screenHeight / 40,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  "8:12 AM",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black,
-                                      fontSize: 15),
-                                ),
-                                const Spacer(),
-                                IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      Constant.toDoModelList.removeAt(index);
-                                    });
-                                  },
-                                  color: Colors.blue,
-                                  icon: const Icon(CupertinoIcons.pen),
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            AddToDo(index: index),
-                                      ),
-                                    ).then((value) {
-                                      setState(() {});
-                                    });
-                                  },
-                                  color: Colors.red,
-                                  icon: const Icon(CupertinoIcons.delete),
-                                ),
-                              ],
-                            ),
-                          ],
+                : Expanded(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      // scrollDirection: Axis.vertical,
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 15),
+                      itemCount: Constant.toDoModelList.length,
+                      itemBuilder: (context, index) => Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                                color: Colors.deepPurple, width: 2.5)),
+                        child: Padding(
+                          padding: EdgeInsets.all(screenWidth / 20),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Title: ${Constant.toDoModelList[index].title}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 22),
+                              ),
+                              SizedBox(
+                                height: screenHeight / 50,
+                              ),
+                              Text(
+                                "content: ${Constant.toDoModelList[index].content}",
+                                // toDoModelList[index].content!,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                    fontSize: 15),
+                              ),
+                              // Text(
+                              //   "time: ${Constant.toDoModelList[index].time}",
+                              //   // toDoModelList[index].content!,
+                              //   style: const TextStyle(
+                              //       fontWeight: FontWeight.w500,
+                              //       color: Colors.black,
+                              //       fontSize: 15),
+                              // ),
+                              SizedBox(
+                                height: screenHeight / 60,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Time: ${Constant.toDoModelList[index].time}",
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black,
+                                        fontSize: 15),
+                                  ),
+                                  const Spacer(),
+                                  IconButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              AddToDo(index: index),
+                                        ),
+                                      ).then((value) {
+                                        setState(() {});
+                                      });
+                                    },
+                                    color: Colors.blue,
+                                    icon: const Icon(CupertinoIcons.pen),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        Constant.toDoModelList.removeAt(index);
+                                      });
+                                    },
+                                    color: Colors.red,
+                                    icon: const Icon(CupertinoIcons.delete),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
